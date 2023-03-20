7479bd7f3d558072a4b3a6068f7f1f26f05fd0f7
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-03-20 19:40:16 +0200

    Update 02-lightbox.js
    Task 2 - SimpleLightbox library

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+let gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML(
+  "afterbegin",
+  galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`
+    )
+    .join("")
+);
+
+new SimpleLightbox(".gallery a", {
+  overlayOpacity: 0.5,
+  captionDelay: 250,
+  captionsData: "alt",
+  fadeSpeed: 250,
+  rtl: true,
+});
